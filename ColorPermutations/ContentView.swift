8a32597ea293653f943a01d29465772c362e6b91
Cesare Forelli <cdf1982@icloud.com>
2022-07-16 10:57:42 +0200

    5 colors view with hardcoded values

--- ColorPermutations/ContentView.swift
+++ ColorPermutations/ContentView.swift
@@ -7,16 +7,36 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 32, style: .continuous)
+                .fill(.gray)
+                .frame(width: 250, height: 250)
+            
+            Circle()
+                .fill(.red)
+                .frame(width: 200, height: 200)
+            
+            Circle()
+                .fill(.green)
+                .frame(width: 150, height: 150)
+            
+            Circle()
+                .fill(.yellow)
+                .frame(width: 100, height: 100)
+            
+            Circle()
+                .fill(.blue)
+                .frame(width: 50, height: 50)
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
