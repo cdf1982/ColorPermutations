7543a7d16dfb3b5f42ea292a2bf86b882d90e6d9
Cesare Forelli <cdf1982@icloud.com>
2022-07-16 10:45:09 +0200

    Initial Commit

---
+++ ColorPermutations/ColorPermutationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPermutationsApp.swift
+//  ColorPermutations
+//
+//  Created by Cesare Forelli on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ColorPermutationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
