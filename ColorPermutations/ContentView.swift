bd58335a16b09ba7343b1611c0b8d3d754e18046
Cesare Forelli <cdf1982@icloud.com>
2022-07-16 11:04:02 +0200

    Refactored view

--- ColorPermutations/ContentView.swift
+++ ColorPermutations/ContentView.swift
@@ -12,31 +12,55 @@ struct ContentView: View {
     
     var body: some View {
         
+        PatternView(
+            rectangle: .gray,
+            largeCircle: .red,
+            bigCircle: .green,
+            mediumCircle: .yellow,
+            smallCircle: .blue
+        )
+    }
+}
+
+
+struct PatternView: View {
+    
+    var rectangle : Color
+    
+    var largeCircle : Color
+    
+    var bigCircle : Color
+    
+    var mediumCircle : Color
+    
+    var smallCircle : Color
+    
+    var body: some View {
+        
         ZStack {
             
             RoundedRectangle(cornerRadius: 32, style: .continuous)
-                .fill(.gray)
+                .fill(rectangle)
                 .frame(width: 250, height: 250)
             
             Circle()
-                .fill(.red)
+                .fill(largeCircle)
                 .frame(width: 200, height: 200)
             
             Circle()
-                .fill(.green)
+                .fill(bigCircle)
                 .frame(width: 150, height: 150)
             
             Circle()
-                .fill(.yellow)
+                .fill(mediumCircle)
                 .frame(width: 100, height: 100)
             
             Circle()
-                .fill(.blue)
+                .fill(smallCircle)
                 .frame(width: 50, height: 50)
         }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
